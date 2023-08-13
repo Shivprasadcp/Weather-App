a9fdf6772e1943df5ee35f1bc59e841b65c43df0
Chavarattil Shivprasad Premarajan <shivprasadcp02@gmail.com>
2023-08-13 14:31:11 +0530

    Adding the API

---
+++ script.js
@@ -0,0 +1,19 @@
+async function getWeather() {
+const url = 'https://weather-by-api-ninjas.p.rapidapi.com/v1/weather?city=Delhi';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '6ba6bf2cd6mshac672a293c417b1p12986fjsn4730f489a732',
+		'X-RapidAPI-Host': 'weather-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}
+}
+ getWeather();
